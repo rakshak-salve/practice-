87f534799611ef4906f77ff7b2a2ea29a24ccba9
gamer-slayer <rakshaksalve0@gmail.com>
2025-03-31 23:59:00 +0000

    overload method

--- MethodOverloadDemo81.java
+++ MethodOverloadDemo81.java
@@ -2,7 +2,11 @@ public class MethodOverloadDemo81 {
     public static int compute(int a) {
         return a * 2;
     }
+    public static int compute(int a, int b) {
+        return a + b;
+    }
     public static void main(String[] args) {
         System.out.println(compute(5));
+        System.out.println(compute(5, 10));
     }
 }
