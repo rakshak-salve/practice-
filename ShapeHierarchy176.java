1b5919e46fdf7096c798b774ae125fdfc9bbef5b
gamer-slayer <rakshaksalve0@gmail.com>
2025-07-04 23:59:00 +0000

    test class instantiation

--- ShapeHierarchy176.java
+++ ShapeHierarchy176.java
@@ -6,4 +6,8 @@ public class ShapeHierarchy176 {
     public String getId() {
         return id;
     }
+    public static void main(String[] args) {
+        ShapeHierarchy176 obj = new ShapeHierarchy176("123");
+        System.out.println(obj.getId());
+    }
 }
