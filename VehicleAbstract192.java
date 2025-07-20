d0c0654892d76da156976f72322121b8d9dc76ae
gamer-slayer <rakshaksalve0@gmail.com>
2025-07-20 13:32:09 +0000

    test class instantiation

--- VehicleAbstract192.java
+++ VehicleAbstract192.java
@@ -6,4 +6,8 @@ public class VehicleAbstract192 {
     public String getId() {
         return id;
     }
+    public static void main(String[] args) {
+        VehicleAbstract192 obj = new VehicleAbstract192("123");
+        System.out.println(obj.getId());
+    }
 }
