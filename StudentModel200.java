c8cf79805d28b952f80dbf690a69afdc7bd2ae6e
gamer-slayer <rakshaksalve0@gmail.com>
2025-07-28 21:10:02 +0000

    test class instantiation

--- StudentModel200.java
+++ StudentModel200.java
@@ -6,4 +6,8 @@ public class StudentModel200 {
     public String getId() {
         return id;
     }
+    public static void main(String[] args) {
+        StudentModel200 obj = new StudentModel200("123");
+        System.out.println(obj.getId());
+    }
 }
