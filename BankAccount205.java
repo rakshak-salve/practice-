1bc3124a1dc0f5e83d8ae80e60b52cf620e02def
gamer-slayer <rakshaksalve0@gmail.com>
2025-08-02 22:07:49 +0000

    test class instantiation

--- BankAccount205.java
+++ BankAccount205.java
@@ -6,4 +6,8 @@ public class BankAccount205 {
     public String getId() {
         return id;
     }
+    public static void main(String[] args) {
+        BankAccount205 obj = new BankAccount205("123");
+        System.out.println(obj.getId());
+    }
 }
