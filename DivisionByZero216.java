ea72a092a8a26f22dbc37b8e8ec0a2e63c8f6b86
gamer-slayer <rakshaksalve0@gmail.com>
2025-08-13 23:59:00 +0000

    add try catch

--- DivisionByZero216.java
+++ DivisionByZero216.java
@@ -1,5 +1,9 @@
 public class DivisionByZero216 {
     public static void main(String[] args) {
         int a = 10, b = 0;
+        try {
+            int c = a / b;
+        } catch(Exception e) {
+        }
     }
 }
