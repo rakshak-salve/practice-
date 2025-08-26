fe086799165270aedf7dc137e2c96ed673fa2190
gamer-slayer <rakshaksalve0@gmail.com>
2025-08-26 22:38:08 +0000

    add try catch

--- CustomExceptionDemo229.java
+++ CustomExceptionDemo229.java
@@ -1,5 +1,9 @@
 public class CustomExceptionDemo229 {
     public static void main(String[] args) {
         int a = 10, b = 0;
+        try {
+            int c = a / b;
+        } catch(Exception e) {
+        }
     }
 }
