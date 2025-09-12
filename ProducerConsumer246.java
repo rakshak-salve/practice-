6db51643076b6a2743a36e489d8626648d95d0a8
gamer-slayer <rakshaksalve0@gmail.com>
2025-09-12 10:12:55 +0000

    start thread in main

--- ProducerConsumer246.java
+++ ProducerConsumer246.java
@@ -2,4 +2,8 @@ public class ProducerConsumer246 implements Runnable {
     public void run() {
         System.out.println("Thread running");
     }
+    public static void main(String[] args) {
+        Thread t = new Thread(new ProducerConsumer246());
+        t.start();
+    }
 }
