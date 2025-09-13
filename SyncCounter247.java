db9fb85b1f474793130725d6a500318072e406a7
gamer-slayer <rakshaksalve0@gmail.com>
2025-09-13 21:50:44 +0000

    start thread in main

--- SyncCounter247.java
+++ SyncCounter247.java
@@ -2,4 +2,8 @@ public class SyncCounter247 implements Runnable {
     public void run() {
         System.out.println("Thread running");
     }
+    public static void main(String[] args) {
+        Thread t = new Thread(new SyncCounter247());
+        t.start();
+    }
 }
