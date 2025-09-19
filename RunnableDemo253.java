759fca74ce99824737870e8055a03e55be46de45
gamer-slayer <rakshaksalve0@gmail.com>
2025-09-19 23:59:00 +0000

    add thread sleep

--- RunnableDemo253.java
+++ RunnableDemo253.java
@@ -1,6 +1,9 @@
 public class RunnableDemo253 implements Runnable {
     public void run() {
-        System.out.println("Thread running");
+        try {
+            Thread.sleep(1000);
+            System.out.println("Thread finished");
+        } catch(InterruptedException e) {}
     }
     public static void main(String[] args) {
         Thread t = new Thread(new RunnableDemo253());
