daaa7ff3c1e3702bbe6be4682cdfa4daf622f059
gamer-slayer <rakshaksalve0@gmail.com>
2025-09-20 23:59:00 +0000

    start thread in main

--- RunnableDemo254.java
+++ RunnableDemo254.java
@@ -2,4 +2,8 @@ public class RunnableDemo254 implements Runnable {
     public void run() {
         System.out.println("Thread running");
     }
+    public static void main(String[] args) {
+        Thread t = new Thread(new RunnableDemo254());
+        t.start();
+    }
 }
