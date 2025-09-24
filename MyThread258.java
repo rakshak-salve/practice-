3ea6c0530044599e9fbf92c56e33b8699768319d
gamer-slayer <rakshaksalve0@gmail.com>
2025-09-24 09:14:43 +0000

    start thread in main

--- MyThread258.java
+++ MyThread258.java
@@ -2,4 +2,8 @@ public class MyThread258 implements Runnable {
     public void run() {
         System.out.println("Thread running");
     }
+    public static void main(String[] args) {
+        Thread t = new Thread(new MyThread258());
+        t.start();
+    }
 }
