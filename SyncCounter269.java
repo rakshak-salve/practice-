9bfe492a70400cfb2b5318794b864130c3f57d33
gamer-slayer <rakshaksalve0@gmail.com>
2025-10-05 21:19:55 +0000

    add thread sleep

--- SyncCounter269.java
+++ SyncCounter269.java
@@ -1,6 +1,9 @@
 public class SyncCounter269 implements Runnable {
     public void run() {
-        System.out.println("Thread running");
+        try {
+            Thread.sleep(1000);
+            System.out.println("Thread finished");
+        } catch(InterruptedException e) {}
     }
     public static void main(String[] args) {
         Thread t = new Thread(new SyncCounter269());
