3ccf2585cf3736f89c588107726a5156460d6069
gamer-slayer <rakshaksalve0@gmail.com>
2025-10-09 13:49:56 +0000

    iterate over collection

--- SetDemo273.java
+++ SetDemo273.java
@@ -4,5 +4,6 @@ public class SetDemo273 {
         List<String> list = new ArrayList<>();
         list.add("Java");
         list.add("Python");
+        for(String s : list) System.out.println(s);
     }
 }
