5f3d5e5228890627916fa8ffd6afd3ece0bb8104
gamer-slayer <rakshaksalve0@gmail.com>
2025-10-09 19:36:11 +0000

    sort collection

--- SetDemo273.java
+++ SetDemo273.java
@@ -4,6 +4,7 @@ public class SetDemo273 {
         List<String> list = new ArrayList<>();
         list.add("Java");
         list.add("Python");
+        Collections.sort(list);
         for(String s : list) System.out.println(s);
     }
 }
