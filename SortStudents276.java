f5e20bd60f4508e9153274afd6f29f8540df5618
gamer-slayer <rakshaksalve0@gmail.com>
2025-10-12 12:24:13 +0000

    add elements to collection

--- SortStudents276.java
+++ SortStudents276.java
@@ -2,5 +2,7 @@ import java.util.*;
 public class SortStudents276 {
     public static void main(String[] args) {
         List<String> list = new ArrayList<>();
+        list.add("Java");
+        list.add("Python");
     }
 }
