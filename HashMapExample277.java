6e3065828107e27ca62e1f65ed3fff9b69735c39
gamer-slayer <rakshaksalve0@gmail.com>
2025-10-13 23:59:00 +0000

    sort collection

--- HashMapExample277.java
+++ HashMapExample277.java
@@ -4,6 +4,7 @@ public class HashMapExample277 {
         List<String> list = new ArrayList<>();
         list.add("Java");
         list.add("Python");
+        Collections.sort(list);
         for(String s : list) System.out.println(s);
     }
 }
