c0b0c21762193df5d0ed74aefb6562c12b81d992
gamer-slayer <rakshaksalve0@gmail.com>
2025-10-14 22:43:00 +0000

    add elements to collection

--- HashMapExample278.java
+++ HashMapExample278.java
@@ -2,5 +2,7 @@ import java.util.*;
 public class HashMapExample278 {
     public static void main(String[] args) {
         List<String> list = new ArrayList<>();
+        list.add("Java");
+        list.add("Python");
     }
 }
