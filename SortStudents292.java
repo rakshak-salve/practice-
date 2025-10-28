b460a0262c4c4602207da9eccee0bf53615d3d0e
gamer-slayer <rakshaksalve0@gmail.com>
2025-10-28 14:11:37 +0000

    iterate over collection

--- SortStudents292.java
+++ SortStudents292.java
@@ -4,5 +4,6 @@ public class SortStudents292 {
         List<String> list = new ArrayList<>();
         list.add("Java");
         list.add("Python");
+        for(String s : list) System.out.println(s);
     }
 }
