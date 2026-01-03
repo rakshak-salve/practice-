2a2fac9bfb6bca54688896c1b0a299f5807a51c7
gamer-slayer <rakshaksalve0@gmail.com>
2026-01-03 16:46:23 +0000

    refactor to pyramid pattern

--- NumberSeries359.java
+++ NumberSeries359.java
@@ -2,9 +2,8 @@ public class NumberSeries359 {
     public static void main(String[] args) {
         int rows = 5;
         for(int i=1; i<=rows; i++) {
-            for(int j=1; j<=i; j++) {
-                System.out.print("* ");
-            }
+            for(int s=1; s<=rows-i; s++) System.out.print(" ");
+            for(int j=1; j<=i; j++) System.out.print("* ");
             System.out.println();
         }
     }
