dd812ba42e6009b5ef2efafcc0ac3267cf1c01b5
gamer-slayer <rakshaksalve0@gmail.com>
2026-01-08 20:53:40 +0000

    add method signature

--- QueueList364.java
+++ QueueList364.java
@@ -1,4 +1,7 @@
 public class QueueList364 {
+    public static void sort(int[] arr) {
+        // sorting logic
+    }
     public static void main(String[] args) {
         int[] arr = {5, 2, 8, 1};
     }
