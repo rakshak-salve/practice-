ac47fda532e53e84c1e2bc477f8da84c0e277d4e
gamer-slayer <rakshaksalve0@gmail.com>
2026-03-17 23:08:48 +0000

    implement sorting/searching logic

--- LinkedListNode432.java
+++ LinkedListNode432.java
@@ -1,8 +1,15 @@
 public class LinkedListNode432 {
     public static void sort(int[] arr) {
-        // sorting logic
+        for(int i=0; i<arr.length-1; i++) {
+            for(int j=0; j<arr.length-i-1; j++) {
+                if(arr[j] > arr[j+1]) {
+                    int temp = arr[j]; arr[j] = arr[j+1]; arr[j+1] = temp;
+                }
+            }
+        }
     }
     public static void main(String[] args) {
         int[] arr = {5, 2, 8, 1};
+        sort(arr);
     }
 }
