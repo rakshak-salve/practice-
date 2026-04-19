66394439bcce614f1044700f794a52c4809113df
gamer-slayer <rakshaksalve0@gmail.com>
2026-04-19 14:45:57 +0000

    add method signature

--- BubbleSortAlgo465.java
+++ BubbleSortAlgo465.java
@@ -1,4 +1,7 @@
 public class BubbleSortAlgo465 {
+    public static void sort(int[] arr) {
+        // sorting logic
+    }
     public static void main(String[] args) {
         int[] arr = {5, 2, 8, 1};
     }
