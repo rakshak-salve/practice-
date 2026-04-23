8b517e266a2609ec618b5c6f7a61aa46b5c4783a
gamer-slayer <rakshaksalve0@gmail.com>
2026-04-23 18:19:10 +0000

    add method signature

--- StackArray469.java
+++ StackArray469.java
@@ -1,4 +1,7 @@
 public class StackArray469 {
+    public static void sort(int[] arr) {
+        // sorting logic
+    }
     public static void main(String[] args) {
         int[] arr = {5, 2, 8, 1};
     }
