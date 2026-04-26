501a4ae255aa878cdb9dcbcf19941487a8826ae6
gamer-slayer <rakshaksalve0@gmail.com>
2026-04-26 14:47:51 +0000

    print array output

--- BubbleSortAlgo472.java
+++ BubbleSortAlgo472.java
@@ -11,5 +11,6 @@ public class BubbleSortAlgo472 {
     public static void main(String[] args) {
         int[] arr = {5, 2, 8, 1};
         sort(arr);
+        for(int i : arr) System.out.print(i + " ");
     }
 }
